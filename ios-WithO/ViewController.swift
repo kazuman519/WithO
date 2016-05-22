62832d63a9ed37c992fb8170195413adabfbf7da
miura <noblesse-oblige_23@i.softbank.jp>
2016-05-23 00:24:35 +0900

    feat(project): to swift

---
+++ ios-WithO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-WithO
+//
+//  Created by 三浦　和真 on 2016/05/23.
+//  Copyright © 2016年 三浦　和真. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
