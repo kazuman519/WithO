e3cfa9c78fafc8e604c0070b0ff0eafc67bfe32f
miura <noblesse-oblige_23@i.softbank.jp>
2016-05-29 18:03:39 +0900

    feat(playground): add try code

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,5 +1,17 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 var str = "Hello, playground"
+
+// ストリーム作成
+let emptyStream: Observable<Int>  = Observable.empty()
+
+// ストリームをサブスクライブする
+let subscription = emptyStream
+    .subscribe {event in print(event)}
+
+let singleElementStream = Observable.just(32)
+let sub = singleElementStream.subscribe{ event in print(event)}
